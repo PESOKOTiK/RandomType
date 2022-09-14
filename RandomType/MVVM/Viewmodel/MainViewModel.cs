cd3df3ade4d2ae377a352ee3c3fbd5ab25fd9e35
Alehey Pylypenko <theaxelakk@gmail.com>
2022-09-14 21:26:38 +0200

    Realized minimize,maximize,close window

--- RandomType/MVVM/Viewmodel/MainViewModel.cs
+++ RandomType/MVVM/Viewmodel/MainViewModel.cs
@@ -4,17 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RandomType.MVVM.Viewmodel
 {
     public class MainViewModel
     {
         public RelayCommand CloseWindowCommand { get; set; }
+        public RelayCommand MaximizeWindowCommand { get;set; }
+        public RelayCommand MinimizeWindowCommand { get;set; }
+
+        public WindowState MinMaxWindow()
+        {
+            if(App.Current.MainWindow.WindowState == System.Windows.WindowState.Maximized)
+            {
+                return System.Windows.WindowState.Normal;
+            }
+            else
+            {
+                return System.Windows.WindowState.Maximized;
+            }
+        }
 
 
         public MainViewModel()
         {
             CloseWindowCommand = new RelayCommand(o => App.Current.Shutdown());
+            MaximizeWindowCommand = new RelayCommand(o=>App.Current.MainWindow.WindowState=MinMaxWindow());
+            MinimizeWindowCommand = new RelayCommand(o => App.Current.MainWindow.WindowState=System.Windows.WindowState.Minimized);
         }
     }
 }
