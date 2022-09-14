df7fb92018954b5393273077861b84aa89b09c2b
Alehey Pylypenko <theaxelakk@gmail.com>
2022-09-14 18:12:07 +0200

    +MVVM folders and files +RelayCommand implementation +Style Template for top buttons

---
+++ RandomType/MVVM/Viewmodel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RandomType.MVVM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomType.MVVM.Viewmodel
+{
+    public class MainViewModel
+    {
+        public RelayCommand CloseWindowCommand { get; set; }
+
+
+        public MainViewModel()
+        {
+            CloseWindowCommand = new RelayCommand(o => App.Current.Shutdown());
+        }
+    }
+}
