c7eb9a18d1485f73d3ce5cba6e8efc5309397144
Alexey Pylypenko <alexey.pylypenko007@gmail.com>
2022-12-08 22:47:13 +0100

    added random english words heneration from txt by characters count

---
+++ RandomType/MVVM/Models/Words.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomType.MVVM.Models
+{
+    public class Words
+    {
+        public string? word;
+        public int lehgth;
+
+        public Words(string Word,int Length)
+        {
+            word=Word;
+            lehgth=Length;
+        }
+
+        public override string ToString()
+        {
+            return word;
+        }
+    }
+}
--- RandomType/MVVM/Viewmodel/MainViewModel.cs
+++ RandomType/MVVM/Viewmodel/MainViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using RandomType.MVVM.Core;
 using RandomType.MVVM.Models;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace RandomType.MVVM.Viewmodel
@@ -13,14 +13,25 @@ namespace RandomType.MVVM.Viewmodel
     {
         public bool isMaximized;
         private CustomTheme selectedTheme;
-
         public CustomTheme SelectedTheme
         {
             get { return selectedTheme; }
             set { selectedTheme = value;
                 OnPropertyChanged(); }
         }
-
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value;OnPropertyChanged(); }
+        }
+        private string hardlevel;
+        public string HardLevel
+        {
+            get { return hardlevel; }
+            set { hardlevel = value; OnPropertyChanged(); }
+        }
+        public ObservableCollection<Words> ghostwords = new ObservableCollection<Words>();
         public bool IsMaximized
         {
             get => isMaximized;
@@ -34,6 +45,7 @@ namespace RandomType.MVVM.Viewmodel
         public RelayCommand MaximizeWindowCommand { get;set; }
         public RelayCommand MinimizeWindowCommand { get;set; }
         public RelayCommand DragMoveCommand { get; set; }
+        public RelayCommand MainCommand { get; set; }
         public WindowState MinMaxWindow()
         {
             if(App.Current.MainWindow.WindowState == System.Windows.WindowState.Maximized)
@@ -51,11 +63,48 @@ namespace RandomType.MVVM.Viewmodel
         public void CanDragMove(){ App.Current.MainWindow.DragMove(); }
         public MainViewModel()
         {
-            IsMaximized=false;
-            CloseWindowCommand = new RelayCommand(o => App.Current.Shutdown());
-            MaximizeWindowCommand = new RelayCommand(o=>App.Current.MainWindow.WindowState=MinMaxWindow());
-            MinimizeWindowCommand = new RelayCommand(o => App.Current.MainWindow.WindowState=System.Windows.WindowState.Minimized);
-            DragMoveCommand = new RelayCommand(o => CanDragMove());
+            Text = "";
+            IsMaximized = false;
+            CloseWindowCommand = new RelayCommand(f => App.Current.Shutdown());
+            MaximizeWindowCommand = new RelayCommand(f => App.Current.MainWindow.WindowState = MinMaxWindow());
+            MinimizeWindowCommand = new RelayCommand(f => App.Current.MainWindow.WindowState = System.Windows.WindowState.Minimized);
+            DragMoveCommand = new RelayCommand(f => CanDragMove());
+            MainCommand = new RelayCommand(f => Main());
+            
+            
+        }
+    
+
+        public void Main()
+        {
+            ghostwords.Clear();
+            using (StreamReader sr = new StreamReader("englishwords.txt"))
+            {
+                int hard = 0;
+                string line;
+                int.TryParse(hardlevel, out hard);
+                if (hard > 3)
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        line = sr.ReadLine();
+                        if (line.Length == hard)
+                        {
+                            ghostwords.Add(new Words(line, line.Length));
+                        }
+                    }
+                }
+
+            };
+            StringBuilder sb = new();
+            for (int i = 0; i < 30; i++)
+            {
+                Random r = new();
+                int max = ghostwords.Count;
+                sb.Append(ghostwords.ElementAt(r.Next(0, max)));
+                sb.Append(" ");
+            }
+            Text = sb.ToString();
         }
     }
 }
